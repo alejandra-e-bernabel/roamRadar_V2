048f4a804770c965fd9eb09db7fd0b9909fbb4f1
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-24 21:19:37 -0400

    Revamped hotels.html. New page will have some funtionality.

--- source/hotel.js
+++ source/hotel.js
@@ -1,6 +1,70 @@
-$(document).ready(function() {
-    $(".btn-search").click(function() {
-        var bookingUrl = "https://www.hotels.com/";
-        window.open(bookingUrl, "_blank");
-    });
-});
+document.addEventListener('DOMContentLoaded', populateSections);
+
+function createSections() {
+    let numNights = document.getElementById('numNights').value;
+    let sectionsContainer = document.getElementById('sectionsContainer');
+
+    sectionsContainer.innerHTML = '';
+
+    for (let i = 0; i < numNights; i++) {
+        let section = document.createElement('section');
+
+        let hotelNameLabel = document.createElement('label');
+        hotelNameLabel.textContent = 'Hotel name:';
+        let hotelNameInput = document.createElement('input');
+        hotelNameInput.type = 'text';
+        hotelNameInput.name = 'hotelName';
+        hotelNameInput.placeholder = 'Enter hotel name';
+        section.appendChild(hotelNameLabel);
+        section.appendChild(hotelNameInput);
+
+        let additionalNotesLabel = document.createElement('label');
+        additionalNotesLabel.textContent = 'Additional notes:';
+        let additionalNotesInput = document.createElement('textarea');
+        additionalNotesInput.name = 'additionalNotes';
+        additionalNotesInput.placeholder = 'Enter additional notes';
+        section.appendChild(additionalNotesLabel);
+        section.appendChild(additionalNotesInput);
+
+        sectionsContainer.appendChild(section);
+    }
+}
+
+function populateSections() {
+    let storedData = JSON.parse(localStorage.getItem('itinerary'));
+
+    if (storedData) {
+        let numNightsInput = document.getElementById('numNights');
+        numNightsInput.value = storedData.length;
+
+        createSections();
+
+        let hotelNameInputs = document.getElementsByName('hotelName');
+        let additionalNotesInputs = document.getElementsByName('additionalNotes');
+
+        for (let i = 0; i < storedData.length; i++) {
+            hotelNameInputs[i].value = storedData[i].hotelName;
+            additionalNotesInputs[i].value = storedData[i].additionalNotes;
+        }
+    }
+}
+
+window.addEventListener('beforeunload', saveSections);
+
+function saveSections() {
+    let hotelNameInputs = document.getElementsByName('hotelName');
+    let additionalNotesInputs = document.getElementsByName('additionalNotes');
+
+    let data = [];
+
+    for (let i = 0; i < hotelNameInputs.length; i++) {
+        let hotelName = hotelNameInputs[i].value;
+        let additionalNotes = additionalNotesInputs[i].value;
+        data.push({
+            hotelName: hotelName,
+            additionalNotes: additionalNotes
+        });
+    }
+
+    localStorage.setItem('itinerary', JSON.stringify(data));
+}
