9ad05195b068025d9f0be0f9bdbc6f6549cb55eb
Alejandra <alejandra.e.bernabel@gmail.com>
2023-11-28 16:10:06 -0500

    updated server.js without API key checks

--- server.js
+++ server.js
@@ -15,34 +15,34 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/location', (req, res) => {
-  const apiKey = process.env.API_KEY;
+// app.get('/location', (req, res) => {
+//   const apiKey = process.env.API_KEY;
 
-  if (!apiKey) {
-      console.error('API key not found');
-      return res.status(500).send('Internal Server Error');
-  }
+//   if (!apiKey) {
+//       console.error('API key not found');
+//       return res.status(500).send('Internal Server Error');
+//   }
 
-  console.log('API Key:', apiKey);
+//   console.log('API Key:', apiKey);
 
-  const locationHtml = path.join(__dirname, '/public/location.html');
-  const apiKeyPlaceholder = 'API_KEY';
-  res.sendFile(locationHtml.replace(apiKeyPlaceholder, apiKey));
-});
+//   const locationHtml = path.join(__dirname, '/public/location.html');
+//   const apiKeyPlaceholder = 'API_KEY';
+//   res.sendFile(locationHtml.replace(apiKeyPlaceholder, apiKey));
+// });
 
-app.get('/restaurant', (req, res) => {
-    const locationHtml = path.join(__dirname, '/public/restaurant.html');
-    const apiKeyPlaceholder = 'API_KEY'
-    const apiKey = process.env.API_KEY
-    res.sendFile(locationHtml.replace(apiKeyPlaceholder, apiKey));
-})
+// app.get('/restaurant', (req, res) => {
+//     const locationHtml = path.join(__dirname, '/public/restaurant.html');
+//     const apiKeyPlaceholder = 'API_KEY'
+//     const apiKey = process.env.API_KEY
+//     res.sendFile(locationHtml.replace(apiKeyPlaceholder, apiKey));
+// })
 
-app.get('/thingsToDo', (req, res) => {
-    const locationHtml = path.join(__dirname, '/public/thingsToDo.html');
-    const apiKeyPlaceholder = 'API_KEY'
-    const apiKey = process.env.API_KEY
-    res.sendFile(locationHtml.replace(apiKeyPlaceholder, apiKey));
-})
+// app.get('/thingsToDo', (req, res) => {
+//     const locationHtml = path.join(__dirname, '/public/thingsToDo.html');
+//     const apiKeyPlaceholder = 'API_KEY'
+//     const apiKey = process.env.API_KEY
+//     res.sendFile(locationHtml.replace(apiKeyPlaceholder, apiKey));
+// })
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
